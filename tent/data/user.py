16ef619666e619e32c16ce9a052d7c61790cdf61
James Ravenscroft <ravenscroftj@gmail.com>
2012-11-20 19:25:28 +0000

    sorted indenting in user module

--- tent/data/user.py
+++ tent/data/user.py
@@ -7,16 +7,16 @@ from sqlalchemy import Column, Integer, String
 from tent.data.dbm import Base, TableMixin
 
 class User(Base,TableMixin):
-  '''
-  User class contains all profile information about a tent user entity
-  '''
+    '''
+    User class contains all profile information about a tent user entity
+    '''
 
-  entity_id  = Column(String, primary_key=True)
-  licenses   = []
-  servers    = []
-  name       = Column(String)
-  avatar_url = Column(String)
-  birthdate  = Column(String)
-  location   = Column(String)
-  gender     = Column(String)
-  bio        = Column(String)
+    entity_id  = Column(String, primary_key=True)
+    licenses   = []
+    servers    = []
+    name       = Column(String)
+    avatar_url = Column(String)
+    birthdate  = Column(String)
+    location   = Column(String)
+    gender     = Column(String)
+    bio        = Column(String)
