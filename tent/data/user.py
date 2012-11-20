59022bd116a2e67c6429942cfaebc3a9c8cea1d1
James Ravenscroft <ravenscroftj@gmail.com>
2012-11-20 18:49:07 +0000

    Basic user model set up

--- tent/data/user.py
+++ tent/data/user.py
@@ -3,17 +3,22 @@ Data model and bindings for tent users
 
 '''
 
-class User:
+from sqlalchemy import Column, Integer, String
+from tent.data.dbm import Base, TableMixin
+
+class User(Base,TableMixin):
   '''
   User class contains all profile information about a tent user entity
   '''
 
-  entity_id  = ""
+  __tablename__ = "users"
+
+  entity_id  = Column(String, primary_key=True)
   licenses   = []
   servers    = []
-  name       = ""
-  avatar_url = ""
-  birthdate  = ""
-  location   = ""
-  gender     = ""
-  bio        = ""
+  name       = Column(String)
+  avatar_url = Column(String)
+  birthdate  = Column(String)
+  location   = Column(String)
+  gender     = Column(String)
+  bio        = Column(String)
