6f744144d99794b1f75ed91214a6667cbb7f8a43
Sam Clements <sam@borntyping.co.uk>
2012-11-28 16:57:26 +0000

    Fix running from command line

--- tentd/__init__.py
+++ tentd/__init__.py
@@ -2,13 +2,41 @@
 
 __version__ = "0.0.0"
 
-import argparse
 from flask import Flask
-
-from tentd.base import base
-from tentd.run import run_application
+from argparse import ArgumentParser
 
 app = Flask('tentd')
+
+from tentd.base import base
+
 app.register_blueprint(base)
 
-run = lambda run: run_application(app)
+def run ():
+	""" Parse the command line arguments and run the application """
+	parser = ArgumentParser(description=__doc__)
+
+	# Basic arguments
+	parser.add_argument("conf", nargs="?",
+		help="a configuration file to use")
+	parser.add_argument("--show", action="store_true",
+		help="show the configuration")
+	parser.add_argument("--norun", action="store_true",
+		help="stop after creating the app, useful with --show")
+
+	# Flask configuration
+	parser.add_argument("--DEBUG", action="store_true",
+		help="run flask in debug mode")
+	
+	args = parser.parse_args()
+	
+	if args.conf:
+		app.config.from_pyfile(args.conf)
+	
+	app.config.from_object(args)
+	
+	if args.show:
+		from pprint import pprint
+		pprint(dict(app.config))
+	
+	if not args.norun:
+		app.run()
