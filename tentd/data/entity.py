5f9a11156806272ffa9b2d5430d3ca92c97ef18b
Alexander Brown <alex@alexanderdbrown.com>
2012-11-28 11:19:05 +0000

    Started adding a table for the Post data, following the tent protocol.

--- tentd/data/entity.py
+++ tentd/data/entity.py
@@ -33,3 +33,29 @@ class Follower (db.Model):
 	Followers are users who have relationships with 
 	'''
 	follower_id = Column(String, primary_key=True)
+
+class Post (db.Model):
+	'''Posts are at the core of Tent. Posts are sent to followers immediately after being 
+	created (from the tent protocol introduction).
+
+	The tent specifcation defines that there are two ways of accessing posts:
+
+	`GET posts/` returns all posts, the parameters of the request define any filtering.
+	There may need to be extra filtering for server-side performance (i.e. only providing a set
+	number of posts to stop the server being overloaded).
+
+	`GET posts/<id>` returns a post with the defined id.'''
+
+	# The UUID of the post on this instance (i.e. the primary key on the DB).
+	post_id = Columm(String, primary_key=True)
+
+	# The type this post is. Valid types are:
+	# - status: a short (<256 character) message.
+	# - essay: a longer message.
+	# - photo: an image.
+	# - album: a collection of photos.
+	post_type = Column(String)
+
+	# The content associated with this post
+	#FIXME This should be a foriegn key to the content type depicted by post_type.
+	content = Column(String)
