444080b5606eed85d4d72dd6000c7649e4321007
Sam Clements <sam@borntyping.co.uk>
2012-11-28 16:57:26 +0000

    Fix running from command line

--- tentd/base.py
+++ tentd/base.py
@@ -1,8 +1,10 @@
 """ The flask application """
 
+from __future__ import absolute_import
+
 from flask import Blueprint, jsonify
 
-from tentd import app, __doc__ as info, __version__
+from tentd import __doc__ as info, __version__
 from tentd.data import db
 
 base = Blueprint('base', __name__)
