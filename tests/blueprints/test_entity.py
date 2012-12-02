b52f0e9fe2c9e41205a8d703985ac39ab3524a8a
Sam Clements <sam@borntyping.co.uk>
2012-12-02 17:27:30 +0000

    Test that the api 404's when a user does not exist

--- tests/blueprints/test_entity.py
+++ tests/blueprints/test_entity.py
@@ -8,16 +8,23 @@ from tentd.models.entity import Entity
 class EntityBlueprintTest (AppTestCase):
 	def setUp (self):
 		super(EntityBlueprintTest, self).setUp()
-		self.user = Entity(name="testuser")
+		self.name = 'testuser'
+		self.user = Entity(name=self.name)
 		db.session.add(self.user)
 		db.session.commit()
 
 	def test_entity_link (self):
-		r = self.client.head("/testuser/")
-		self.assertIn("/testuser/profile", r.headers['Link'])
-
+		r = self.client.head('/' + self.name)
+		self.assertIn('/' + self.name + 'profile', r.headers['Link'])
+	
+	def test_entity_link_404 (self):
+		self.assertStatus(self.client.head('/non-existent-user'), 404)
+	
+	def test_entity_profile_404 (self):
+		self.assertStatus(self.client.head('/non-existent-user/profile'), 404)
+	
 	def test_entity_profile_json (self):
-		r = self.client.get("/testuser/profile")
+		r = self.client.get('/testuser/profile')
 		
 		self.assertEquals(r.mimetype, 'application/json')
 		self.assertIn('https://tent.io/types/info/core/v0.1.0', r.json)
