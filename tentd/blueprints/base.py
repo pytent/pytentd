843816b07ba926c80838adac680f406c50036954
Sam Clements <sam@borntyping.co.uk>
2012-12-02 17:26:52 +0000

    Move entity link into the base blueprint

--- tentd/blueprints/base.py
+++ tentd/blueprints/base.py
@@ -1,6 +1,6 @@
 """ The flask application """
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, url_for, make_response
 
 from tentd import __version__, __doc__ as info
 
@@ -9,3 +9,9 @@ base = Blueprint('base', __name__)
 @base.route('/')
 def the_docstring ():
 	return jsonify(info=info, version=__version__)
+
+@base.route('/<entity:entity>', endpoint='link', methods=['HEAD'])
+def get_user (entity):
+	resp = make_response()
+	resp.headers['Link'] = url_for('entity.profile', entity=entity, _external=True)
+	return resp
--- tentd/blueprints/entity.py
+++ tentd/blueprints/entity.py
@@ -2,18 +2,12 @@
 
 from functools import wraps
 
-from flask import Blueprint, jsonify, url_for, make_response
+from flask import Blueprint, jsonify
 
 from tentd.models import db
 from tentd.models.entity import Entity, CoreProfile, BasicProfile
 
 entity = Blueprint('entity', __name__, url_prefix='/<entity:entity>')
-
-@entity.route('/', endpoint='link', methods=['HEAD'])
-def get_user (entity):
-	resp = make_response()
-	resp.headers['Link'] = url_for('.profile', entity=entity, _external=True)
-	return resp
 
 @entity.route('/profile', endpoint='profile')
 def profile (entity):
