8fece1ad288613b3633de1148957b56379a36b74
Sam Clements <sam@borntyping.co.uk>
2012-12-20 17:57:58 +0000

    Added new-user and clean commands to the CLI
    * `CONF` argument has become `--conf CONF`

--- tentd/cli.py
+++ tentd/cli.py
@@ -4,14 +4,14 @@ import os
 import signal
 
 from argparse import ArgumentParser
-from flask import Flask, Config
+from flask import Config
 
-from tentd import create_app
+from tentd import create_app, db
 
 parser = ArgumentParser(description=__doc__)
 
 # Basic arguments
-parser.add_argument("conf", nargs="?",
+parser.add_argument("--conf",
     help="a configuration file to use")
 parser.add_argument("--show", action="store_true",
     help="show the configuration")
@@ -31,8 +31,9 @@ parser.add_argument('--db-echo', action='store_true',
 
 subparsers = parser.add_subparsers(dest="subcommand")
 
-# Daemon mode
-daemon = subparsers.add_parser('daemon', help='run pytentd as a daemon')
+# Manage daemon mode
+daemon = subparsers.add_parser('daemon',
+    help='run pytentd as a daemon')
 daemon.add_argument("--stop", "--kill", action="store_true",
     help="kill an existing daemon process")
 daemon.add_argument("--start", action="store_true",
@@ -40,39 +41,7 @@ daemon.add_argument("--start", action="store_true",
 daemon.add_argument("--status", action="store_true",
     help="show the status of the pytentd daemon")
 
-subparsers.add_parser('start', help="start a pytentd server")
-
-def run():
-    """Parse the command line arguments and run the application"""
-    args = parser.parse_args()
-    config = Config(os.getcwd())
-
-    # Set the configuration options from the file given
-    if args.conf:
-        config.from_pyfile(args.conf)
-
-    # Load the rest of the arguments, overriding the conf file
-    config['DEBUG'] = args.debug or args.dev
-    config['TESTING'] = args.testing or args.dev
-    config['SQLALCHEMY_ECHO'] = args.db_echo
-
-    # Create the application and create the database
-    app = create_app(config)
-
-    if args.show:
-        from pprint import pprint
-        pprint(dict(app.config))
-        print vars(args)
-
-    # Manage daemon mode
-    if args.subcommand == 'daemon':
-        daemonize(args)
-
-    # Run the application
-    elif args.subcommand == 'start':
-        app.run()
-
-def daemonize(args):
+def daemonize_command(app, args):
     from daemonize import Daemonize
 
     def read_pid (pidfile):
@@ -119,3 +88,66 @@ def daemonize(args):
             daemon.start()
         else:
             print "A pytentd daemon is already running [{}]".format(pid)
+
+# Add a new user
+new_user = subparsers.add_parser('new-user',
+    help='create a new user')
+new_user.add_argument("name",
+    help="the name to use in urls for the user")
+
+def new_user_command(app, args):
+    from tentd.models.entity import Entity
+
+    db.app = app
+    entity = Entity(name=args.name)
+    db.session.add(entity)
+    db.session.commit()
+
+    print "Created new entity \"{}\" [{}]".format(entity.name, entity.id)
+
+# Clear the db
+clean = subparsers.add_parser('clean',
+    help="clean the db")
+
+def clean_command(app, args):
+    db.app = app
+    db.drop_all()
+    print "Dropped all tables"
+
+# Start a server instance
+subparsers.add_parser('start',
+    help="start a pytentd server")
+
+def start_command(app, args):
+    app.run()
+
+def run():
+    """Parse the command line arguments and run the application"""
+    args = parser.parse_args()
+    config = Config(os.getcwd())
+
+    # Set the configuration options from the file given
+    if args.conf:
+        config.from_pyfile(args.conf)
+
+    # Load the rest of the arguments, overriding the conf file
+    config['DEBUG'] = args.debug or args.dev
+    config['TESTING'] = args.testing or args.dev
+    config['SQLALCHEMY_ECHO'] = args.db_echo
+
+    # Create the application and create the database
+    app = create_app(config)
+
+    # Show the application config
+    if args.show:
+        from pprint import pprint
+        pprint(dict(app.config))
+        print vars(args)
+
+    # Run the function named 'subcommand_command'
+    # Any dashes in the subcommand name are replaced with underscores
+    try:
+        command_name = args.subcommand.replace('-', '_') + '_command'
+        globals()[command_name](app, args)
+    except:
+        raise Exception("No such command")
