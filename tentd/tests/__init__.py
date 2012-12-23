465ac99e0c1536ca92857224c1d164922703c06c
Sam Clements <sam@borntyping.co.uk>
2012-12-23 19:24:48 +0000

    tentd.tests.blueprints.test_entity

--- tentd/tests/__init__.py
+++ tentd/tests/__init__.py
@@ -1,12 +1,16 @@
-"""Tests for pytentd"""
+"""Tests for pytentd
 
-__all__ = ['AppTestCase', 'skip']
+Also provides some imports from the testing libraries used.
+"""
+
+__all__ = ['TentdTestCase', 'MockResponse', 'MockFunction', 'patch' 'skip']
 
 from os import close, remove
 from tempfile import mkstemp
 from unittest import TestCase, skip
 
-from flask import Response, json_available, json, make_response, jsonify, _request_ctx_stack
+from flask import json, jsonify, Response, _request_ctx_stack
+from mock import NonCallableMock, patch
 from werkzeug import cached_property
 
 from tentd import create_app, db
@@ -14,15 +18,46 @@ from tentd import create_app, db
 class TestResponse(Response):
     @cached_property
     def json(self):
-        if not json_available:
-            raise NotImplementedError(
-                "You! What are you doing? You! Staph!"
-                "Trying to run Pytentd without json is not going to work.")
-        elif not self.mimetype == 'application/json':
-            return None
-        return json.loads(self.data)
+        if self.mimetype == 'application/json':
+            return json.loads(self.data)
+        return None
 
-class AppTestCase(TestCase):
+class MockResponse(NonCallableMock):
+    """A mock response, for use with MockFunction"""
+
+    #: Use a default status code
+    status_code = 200
+
+    #: The argument the response is for
+    __argument__ = None
+
+    def __str__(self):
+        return "<MockResponse for {}>".format(self.__argument__)
+
+class MockFunction(dict):
+    """A callable argument->value dictionary for patching over a function
+
+    New argument->value pairs can be assigned in the same way as a dict,
+    and values can be returned by calling it as a function.
+
+        with mock.patch('requests.head', new_callable=MockFunction) as head:
+            head['http://example.com'] = MockResponse(data="Hello world.")
+            ...
+    """
+
+    def __call__(self, argument):
+        """Return the value, setting it's __argument__ attribute"""
+        if argument in self:
+            self[argument].__argument__ = argument
+            return self[argument]
+        raise KeyError("No mock response set for '{}'".format(argument))
+
+    def __repr__(self):
+        return "{}({})".format(
+            self.__class__.__name__,
+            super(MockFunction, self).__repr__())
+
+class TentdTestCase(TestCase):
     """A base test case for pytentd
 
     It handles setting up the app and request contexts
