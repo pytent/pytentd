75bf7d8c9c697fa12ee1fcf0a2420ebc842fd6d0
Alex Brown <alex@alexanderdbrown.com>
2013-01-15 12:08:47 +0000

    Full conversion to MethodView style in the entity blueprints. This should make our life easier.

--- tentd/blueprints/entity.py
+++ tentd/blueprints/entity.py
@@ -14,8 +14,12 @@ entity = Blueprint('entity', __name__, url_prefix='/<string:entity>')
 
 @entity.route_class('')
 class EntityView(MethodView):
+    """The base view for entities."""
+
     endpoint = 'deafult'
+
     def head(self, entity, **kargs):
+        """Returns the entity link header."""
         link = '<{url}>; rel="https://tent.io/rels/profile"'.format(
             url=url_for('entity.profile', entity=entity.name, _external=True))
         resp = jsonify(entity.to_json())
@@ -29,27 +33,37 @@ def fetch_entity(endpoint, values):
     """Replace `entity` (which is a string) with the actuall entity"""
     values['entity'] = Entity.objects.get_or_404(name=values['entity'])
 
-@entity.route('/profile')
-def profile(entity):
-    """Return the info types belonging to the entity"""
-    return jsonify({p.schema: p.to_json() for p in entity.profiles})
+@entity.route_class('/profile')
+class ProfileView(EntityView):
+    """The view for profile-based routes."""
+    endpoint = 'profile'
+    def get(self, entity):
+        """Return the info types belonging to the entity"""
+        return jsonify({p.schema: p.to_json() for p in entity.profiles})
 
-@entity.route('/followers', methods=['POST'])
-def followers(entity):
-    """Starts following a user, defined by the post data"""
-    try:
-        post_data = json.loads(request.data)
-    except json.JSONDecodeError as e:
-        raise APIBadRequest(str(e))
+@entity.route_class('/followers')
+class FollowersView(EntityView):
+    """View for followers-based routes."""
 
-    if not post_data:
-        raise APIBadRequest("No POST data.")
+    endpoint='followers'
+
+    def post(self, entity):
+        """Starts following a user, defined by the post data"""
+        try:
+            post_data = json.loads(request.data)
+        except json.JSONDecodeError as e:
+            raise APIBadRequest(str(e))
+
+        if not post_data:
+            raise APIBadRequest("No POST data.")
     
-    follower = follow.start_following(entity, post_data)
-    return jsonify(follower.to_json())
+        follower = follow.start_following(entity, post_data)
+        return jsonify(follower.to_json())
 
 @entity.route_class('/followers/<string:follower_id>')
 class FollowerView(EntityView):
+    """View for follower-based routes."""
+
     endpoint = 'follower'
     
     def get(self, entity, follower_id):
@@ -57,6 +71,7 @@ class FollowerView(EntityView):
         return jsonify(entity.followers.get_or_404(id=follower_id).to_json())
 
     def put(self, entity, follower_id):
+        """Updates a following relationship."""
         try:
             post_data = json.loads(request.data)
         except json.JSONDecodeError as e:
@@ -65,16 +80,18 @@ class FollowerView(EntityView):
         return jsonify(updated_follower.to_json())
 
     def delete(self, entity, follower_id):
+        """Deletes a following relationship."""
         try:
             follow.stop_following(entity, follower_id)
             return '', 200
         except ValidationError:
             raise APIBadRequest("The given follower id was invalid")
 
-@entity.route('/notification', methods=['GET'])
-def get_notification(entity):
-    """ Alerts of a notification """
-    return '', 200
+@entity.route_class('/notification')
+class NotificationView(EntityView):
+    def post(self, entity):
+        """ Alerts of a notification """
+        return '', 200
 
 @entity.route_class('/posts')
 class PostView(EntityView):
