106eafeb00b9bf797fd4f8f332607bde3dd85fba
James Ravenscroft <ravenscroftj@gmail.com>
2013-01-19 09:23:47 +0000

    Started working on model for keypairs in mongo

---
+++ tentd/documents/auth.py
@@ -0,0 +1,17 @@
+"""
+Data model for tent auth
+"""
+
+from mongoengine import *
+
+from tentd.documents import db
+
+class KeyPair(db.Document):
+    """KeyPair objects are used to store mac id/key pairs for signing requests"""
+
+    mac_id = StringField(max_length=32,unique=True)  
+    mac_key = StringField(max_length=64)
+    mac_algorithm = StringField()
+
+    def __init__(self, **kwargs):
+        super(KeyPair, self).__init__(**kwargs)
