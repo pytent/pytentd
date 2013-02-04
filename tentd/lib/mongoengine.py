9ac85208c65c361abbb984db51f87074ac08b988
Sam Clements <sam@borntyping.co.uk>
2013-02-04 18:07:34 +0000

    Made tentd.lib.mongoengine use absolute_import to avoid import errors

--- tentd/lib/mongoengine.py
+++ tentd/lib/mongoengine.py
@@ -1,7 +1,11 @@
-__all__ = ['URIField']
+"""Additions to mongoengine"""
+
+from __future__ import absolute_import
 
 from rfc3987 import get_compiled_pattern
-from mongoengine.fields import URLField
+from mongoengine import URLField
+
+__all__ = ['URIField']
 
 class URIField(URLField):
     """An inproved version of mongoengine.URLField"""
