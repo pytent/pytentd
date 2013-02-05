94a4e641a59be561bc6897c2770ca2b21c9fc0e0
Sam Clements <sam@borntyping.co.uk>
2013-02-05 21:09:49 +0000

    Ignore some endpoints when loading g.entity
    This is so endpoints that don't use g.entity don't 404 in single user mode.
    When not in single user mode, we check that the endpoint is expecting an entity.

--- tentd/app.py
+++ tentd/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, current_app, g, url_for
 from mongoengine import ValidationError
-from werkzeug.exceptions import ImATeapot
+from werkzeug.exceptions import ImATeapot, NotFound
 
 from tentd import __doc__ as description, __version__ as version
 from tentd.lib.flask import Request, Response, JSONEncoder, jsonify
@@ -21,6 +21,9 @@ class TentdFlask(Flask):
 
     #: Not useful until Flask 0.10
     json_encoder = JSONEncoder
+
+    #: Endpoints that we can skip trying to load an entity for
+    blank_endpoints = ('home', 'coffee')
     
     def __init__(self, *args, **kwargs):
         """Initialise the application normally and then register several
@@ -28,9 +31,9 @@ class TentdFlask(Flask):
         super(TentdFlask, self).__init__(*args, **kwargs)
 
         # Url handlers, mostly used to implement single user mode
-        self.url_value_preprocessor(self._assign_global_entity)
-        self.after_request(self._add_link_to_response)
-        self.url_defaults(self._url_defaults_entity)
+        self.url_value_preprocessor(self._url_value_preprocessor)
+        self.after_request(self._after_request)
+        self.url_defaults(self._url_defaults)
 
         # Error handlers
         self.errorhandler(ValidationError)(self._validation_error)
@@ -38,7 +41,7 @@ class TentdFlask(Flask):
     @property
     def single_user_mode(self):
         """Returns the value of SINGLE_USER_MODE, defaulting to False"""
-        return self.config.get('SINGLE_USER_MODE', False)
+        return self.config.get('SINGLE_USER_MODE', None)
     
     def register_entity_blueprint(self, blueprint, **kwargs):
         """Register a blueprint with the /<entity> prefix if needed"""
@@ -46,13 +49,26 @@ class TentdFlask(Flask):
         url_prefix += blueprint.url_prefix or ''
         self.register_blueprint(blueprint, url_prefix=url_prefix, **kwargs)
 
-    def _assign_global_entity(self, endpoint, values):
-        """Assigns g.entity using app.single_user_mode or the url value"""
-        if endpoint and 'entity' in values or self.single_user_mode:
-            name = values.pop('entity', None) or self.single_user_mode
-            g.entity = Entity.objects.get_or_404(name=name)
+    def _assign_global_entity(self, name):
+        """Set g.entity using an entity name"""
+        try:
+            g.entity = Entity.objects.get(name=name)
+        except Entity.DoesNotExist:
+            raise NotFound("The entity reqested does not exist")
+        
+    def _url_value_preprocessor(self, endpoint, values):
+        """Work out the entity that is currently in use"""
+        if endpoint and endpoint not in self.blank_endpoints:
+            # If the endpoint is expecting an entity value,
+            # use that to fetch the entity
+            if self.url_map.is_endpoint_expecting(endpoint, 'entity'):
+                self._assign_global_entity(values.pop('entity'))
+            # If we are in single user mode, use that as the name
+            # if the endpoint is not marked
+            elif self.single_user_mode:
+                self._assign_global_entity(self.single_user_mode)
 
-    def _add_link_to_response(self, response):
+    def _after_request(self, response):
         """Add the link header to the response if the entity is set"""
         if hasattr(g, 'entity'):
             link = '<{}>; rel="https://tent.io/rels/profile"'.format(
@@ -60,7 +76,7 @@ class TentdFlask(Flask):
             response.headers['Link'] = link
         return response
     
-    def _url_defaults_entity(self, endpoint, values):
+    def _url_defaults(self, endpoint, values):
         """Adds the entity to calls to url_for if it has been set"""
         if self.url_map.is_endpoint_expecting(endpoint, 'entity'):
             if 'entity' not in values and hasattr(g, 'entity'):
