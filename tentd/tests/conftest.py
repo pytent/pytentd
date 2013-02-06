cf7bb626214e6245de60af91512bd97f169f2a6e
Sam Clements <sam@borntyping.co.uk>
2013-02-06 12:52:28 +0000

    Added better handling of application modes (--mode [single,multi,both])
    * Collections are dropped after all tests that use the app fixture
    * Fixtures no longer need to call drop_collections() as a finalizer

--- tentd/tests/conftest.py
+++ tentd/tests/conftest.py
@@ -9,23 +9,13 @@ from werkzeug import ImmutableDict
 from tentd import create_app
 from tentd.documents import collections, Entity
 
-logging.basicConfig(level=logging.INFO)
-
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('testing')
 
-def pytest_runtest_call(item):
-    """Log each test"""
-    log.info("Running {}".format(item.obj.__name__))
+#: Constant values for single and multi user modes
+MULTIPLE, SINGLE = 'multiple user mode', 'single user mode'
 
-def drop_collections():
-    """Function to drop all of the collections used by pytentd"""
-    log.debug("Dropping collections")
-    for collection in collections:
-        collection.drop_collection()
-
-    for collection in collections:
-        assert collection.objects.count() == 0
-
+#: The configuration used to create instances of the tentd app
 DEFAULT_APP_CONFIGURATION = ImmutableDict({
     'DEBUG': True,
     'TESTING': True,
@@ -37,11 +27,36 @@ DEFAULT_APP_CONFIGURATION = ImmutableDict({
     },
 })
 
-MULTIPLE, SINGLE = 'Multiple user mode', 'Single user mode'
+def pytest_addoption(parser):
+    """Add an option to chose the modes tests will be run under"""
+    parser.addoption("--mode", action="store", default="both",
+        help="the mode to run pytentd in - multi, single, or both")
 
-@fixture(scope="module", params=[MULTIPLE, SINGLE])
+def pytest_generate_tests(metafunc):
+    """Apply the --mode option to the app fixture"""
+    if 'app' in metafunc.fixturenames:
+        metafunc.parametrize('app', {
+            'multi': (MULTIPLE,),
+            'single': (SINGLE,),
+            'both': (MULTIPLE, SINGLE),
+        }[metafunc.config.option.mode], indirect=True, scope="module")
+    
+def pytest_runtest_call(item):
+    """Log each test"""
+    log.info("Running {}".format(item.obj.__name__))
+
+def pytest_runtest_teardown(item, nextitem):
+    """If the app fixture was used, clear the database after the test"""
+    if 'app' in item.fixturenames:
+        log.debug("Dropping collections")
+        for collection in collections:
+            collection.drop_collection()
+
+@fixture
 def app(request):
-    """Create an instance of the Tentd app prepared for testing"""
+    """Create an instance of the Tentd app prepared for testing
+
+    The scope for this fixture is defined in pytest_generate_tests"""
     configuration = DEFAULT_APP_CONFIGURATION.copy()
     if request.param == SINGLE:
         configuration['SINGLE_USER_MODE'] = "single_user_name"
@@ -54,20 +69,13 @@ def app(request):
     ctx.push()
     request.addfinalizer(ctx.pop)
 
-    # Drop all collections when finished
-    request.addfinalizer(drop_collections)
-
     log.info("Created pytentd application[{}]".format(request.param))
 
     return app
 
 @fixture(scope="function")
 def entity(request, app):
-    entity = Entity.new(
+    return Entity.new(
         name=app.single_user_mode or 'testuser',
         identity= "http://example.com",
-        servers=["http://tent.example.com"])
-
-    request.addfinalizer(drop_collections)
-    
-    return entity.save()
+        servers=["http://tent.example.com"]).save()
