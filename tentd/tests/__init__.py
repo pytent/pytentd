759c6c416fca4f52fb1e6039f905eeadaa55535f
Sam Clements <sam@borntyping.co.uk>
2013-02-09 10:46:21 +0000

    Added secure option to HTTP

--- tentd/tests/__init__.py
+++ tentd/tests/__init__.py
@@ -4,20 +4,30 @@ __all__ = ['GET', 'PUT', 'POST', 'HEAD']
 
 from flask import current_app, json, url_for, g
 from py.test import main
+from werkzeug.datastructures import ImmutableDict, Headers
 
 from tentd.lib.flask import JSONEncoder
 
 class HTTP(object):
     """HTTP convenience functions that take an endpoint name,
     and optionally data or JSON to send"""
+
+    macid  = "s:f5949a1d"
+    tstamp = 1355181298
+    nonce  = "b07235"
+    mac    = "swgy4RpdIBaFpA1hmAbZrph24VVg9FwmJgMm9GkgiLE="
     
     def __init__(self, function_name):
         """Creates a HTTP function using the name of the method that will be
         called on ``current_app.client``"""
         self.function_name = function_name
 
+    def authentication_header(self):
+        return 'MAC id="{0}",ts="{1}",nonce="{2}",mac="{3}"'.format(
+            self.macid, self.tstamp, self.nonce, self.mac)
+
     def __call__(self, endpoint, data=None, secure=False,
-        content_type='text/html', **kwargs):
+        headers=None, content_type='text/html', **kwargs):
         """Call current_app.client.<function> and return the response.
 
         The endpoint argument is used to build an url using url_for, along
@@ -34,15 +44,20 @@ class HTTP(object):
             raise NotImplementedError(
                 "The application requires a test client")
 
+        headers = headers or Headers()
+        
+        # Emulate a HMAC request if needed
         if secure:
-            raise NotImplementedError(
-                "Secure requests are not yet implemented")
+            headers.set('Authorization', self.authentication_header())
 
         # Fetch and call the function from the client
         http_function = getattr(current_app.client, self.function_name)
-        return http_function(endpoint, data=data, content_type=content_type)
+        return http_function(
+            endpoint, data=data, headers=headers, content_type=content_type)
 
-GET, PUT, POST, HEAD = HTTP('get'), HTTP('put'), HTTP('post'), HTTP('head')
+# Generate the HTTP Methods
+for method_name in ('delete', 'get', 'head', 'post', 'put'):
+    locals()[method_name.upper()] = HTTP(method_name)
 
 def profile_url_for(entity, _external=False):
     """Get an entity profile url without using url_for"""
